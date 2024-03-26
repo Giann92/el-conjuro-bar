b98de643fa324f11d7cd3185f409edaaeac27302
Giann92 <desumma.gianfranco@gmail.com>
2024-03-26 01:43:31 -0300

    modific-varias

--- components/ui/photoGallery.tsx
+++ components/ui/photoGallery.tsx
@@ -22,7 +22,7 @@ const media = importAll(require.context('../../public/images/', false, /\.(png|j
 const ImageGallery: React.FC = () => {
   // console.log('Media:', media);
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-8 gap-2 md:gap-4 lg:gap-1">
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-8 gap-2 md:gap-4 lg:gap-1">
       {media.map((item: { type: string, src: string, filename: string }, index: number) => (
         <div key={index} className="relative aspect-w-1 aspect-h-1">
           <img src={`/images/${item.filename}`} alt={`Image ${index + 1}`} className="w-full h-full object-cover rounded-lg" />
